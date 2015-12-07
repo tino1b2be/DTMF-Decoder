90cc845c4d22e4b9168381a9103e94dfb951ada6
tino1b2be <ttchemvura@gmail.com>
2015-12-07 13:46:13 +0200

    Added frequence tolerances accoding to ITU standards (in transformFrames.m function). Also changed the license to MIT.

--- Prototyping/getRawKeys.m
+++ Prototyping/getRawKeys.m
@@ -1,17 +1,27 @@
-% Copyright (C) 2015 Tinotenda Chemvura
+% Copyright (c) 2015 Tinotenda Chemvura
 % 
-% This program is free software; you can redistribute it and/or modify it
-% under the terms of the GNU General Public License as published by
-% the Free Software Foundation; either version 3 of the License, or
-% (at your option) any later version.
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
 % 
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
 % 
-% You should have received a copy of the GNU General Public License
-% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+% 
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
+%
+% http://opensource.org/licenses/MIT
+%
 
 % -*- texinfo -*- 
 % @deftypefn {Function File} {@var{retval} =} makeFrames (@var{input1}, @var{input2})
@@ -68,8 +78,8 @@ function rawKeys = getRawKeys( dft_data  )
             rawKeys(j) = '_';
             
         else
-            [mag,low] = max(dft_data(1:4,j));
-            [mag,high] = max(dft_data(5:8,j));
+            [a,low] = max(dft_data(1:4,j));
+            [a,high] = max(dft_data(5:8,j));
 
             % find the corresponding frequencies
             if (low == 1) %low = 697
