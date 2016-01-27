5d80e217dd90a3032b39a77a38f6291462242e24
tino1b2be <ttchemvura@gmail.com>
2016-01-27 10:56:58 +0200

    Fixed gradle build script

--- source/com/tino1b2be/guiprograms/app/DTMFDecoderGUI.java
+++ source/com/tino1b2be/guiprograms/app/DTMFDecoderGUI.java
@@ -7,10 +7,9 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JMenu;
 
 public class DTMFDecoderGUI {
 
--- source/com/tino1b2be/guiprograms/applet/DTMF_Decoder.java
+++ source/com/tino1b2be/guiprograms/applet/DTMF_Decoder.java
@@ -1,6 +1,10 @@
 package com.tino1b2be.guiprograms.applet;
 
 import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
 
 public class DTMF_Decoder extends JApplet {
 
@@ -8,7 +12,37 @@ public class DTMF_Decoder extends JApplet {
 	 * Create the applet.
 	 */
 	public DTMF_Decoder() {
+		getContentPane().setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 450, 21);
+		getContentPane().add(menuBar);
+		
+		JMenu mnMenu = new JMenu("Menu");
+		menuBar.add(mnMenu);
+		
+		JMenuItem mntmDecodeDtmf = new JMenuItem("Decode DTMF");
+		mnMenu.add(mntmDecodeDtmf);
+		
+		JMenuItem mntmGenerateDtmf = new JMenuItem("Generate DTMF");
+		mnMenu.add(mntmGenerateDtmf);
+		
+		JMenu mnAbout = new JMenu("About");
+		menuBar.add(mnAbout);
+		
+		JMenuItem mntmLicense = new JMenuItem("License");
+		mnAbout.add(mntmLicense);
+		
+		JMenuItem mntmAbout = new JMenuItem("About");
+		mnAbout.add(mntmAbout);
+		
+		JButton btnDecodeDtmf = new JButton("Decode DTMF");
+		btnDecodeDtmf.setBounds(12, 49, 179, 74);
+		getContentPane().add(btnDecodeDtmf);
+		
+		JButton btnGenrateDtmf = new JButton("Generate DTMF");
+		btnGenrateDtmf.setBounds(203, 49, 179, 74);
+		getContentPane().add(btnGenrateDtmf);
 
 	}
-
 }
