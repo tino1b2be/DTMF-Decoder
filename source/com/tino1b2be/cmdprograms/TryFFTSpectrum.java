c2e85f054c2d5ebcc244af62216c5815528142b3
Tinotenda Chemvura <ttchemvura@gmail.com>
2016-12-29 15:04:36 +0200

    Modified the FFT program to be more generic.

--- source/com/tino1b2be/cmdprograms/TryFFTSpectrum.java
+++ source/com/tino1b2be/cmdprograms/TryFFTSpectrum.java
@@ -5,23 +5,19 @@ import org.apache.commons.math3.transform.DftNormalization;
 import org.apache.commons.math3.transform.FastFourierTransformer;
 import org.apache.commons.math3.transform.TransformType;
 
-import com.tino1b2be.dtmfdecoder.DecoderUtil;
-import com.tino1b2be.dtmfdecoder.Signals;
-
 public class TryFFTSpectrum {
 
 	public static void main(String[] args) {
-		double startT = System.currentTimeMillis();
 		final double Fs = 8000;
 		final int N = 256;
 
-		// *** Generate a signal
+		// *** Generate a signal with frequency f
 		double[] signal = new double[N];
 		System.out.println("Frame length: " + N + " samples => " + (N / Fs) + "ms");
 
-		final double f1 = 697.0, f2 = 1209; // Hz
+		final double f = 1234.0; // Hz
 		for (int ii = 0; ii < N; ii++) {
-			signal[ii] = Math.sin(2.0 * Math.PI * f1 * (double) ii / Fs) + Math.sin(2.0 * Math.PI * f2 * (double) ii / Fs) ;
+			signal[ii] = Math.sin(2.0 * Math.PI * f * (double) ii / Fs);
 		}
 
 		// Should apply hamming window to the frame.
@@ -29,7 +25,6 @@ public class TryFFTSpectrum {
 		// *** Get power spectrum
 		final FastFourierTransformer fft = new FastFourierTransformer(DftNormalization.STANDARD);
 
-		
 		// Note: signal.length should have been a power of 2
 		final Complex[] spectrum = fft.transform(signal, TransformType.FORWARD);
 		final double[] powerSpectrum = new double[N / 2 + 1];
@@ -40,27 +35,18 @@ public class TryFFTSpectrum {
 
 		// Expect a sharp peak at around frequency f - index f/Fs *
 		// signal.length
-		final double center1 = (f1 / Fs) * N;
-		final double center2 = (f2 / Fs) * N;
-		
-		int start, end;
-//		start = (int) (center - 10.0);
-//		end = (int) (center + 10.0);
-		start = 0;
-		end = powerSpectrum.length;
-		
-		System.out.println("Center frequency 1 in the FFT: " + center1);
-		System.out.println("Center frequency 2 in the FFT: " + center2);
+		final double center = (f / Fs) * N;
+		final int start = (int) (center - 10.0);
+		final int end = (int) (center + 10.0);
+
+		System.out.println("Center frequency in the FFT: " + center);
 		for (int ii = start; ii < end; ii++) {
 			System.out.format("% 3d (% 4.0fHz) power:%01.01f\n", ii, ii * Fs / N, powerSpectrum[ii]);
 		}
 
 		// *** Detect
 		double totalPower = sum(powerSpectrum);
-		double totalPower2 = Signals.power(signal);
-		double totalPower3 = DecoderUtil.signalPower(signal);
-		
-		double signalPower = powerSpectrum[32] + powerSpectrum[31]  + powerSpectrum[33]+ powerSpectrum[55] + powerSpectrum[56] + powerSpectrum[57];
+		double signalPower = powerSpectrum[39] + powerSpectrum[40];
 		double ratio = signalPower / totalPower;
 
 		// around 80%. Reason being leakage in the FFT. The actual spectrum is a
@@ -69,16 +55,12 @@ public class TryFFTSpectrum {
 		// lobe. It's ok though, it just affects the width of the band where you
 		// look for the actual frequency of interest.
 		System.out.println("Detection ratio: " + ratio);
-		
-		double stopT = System.currentTimeMillis();
-		System.out.println("Time taken = " + Double.toString((stopT-start)/1000) + "sec.");
 	}
 
 	private static double sum(double[] powerSpectrum) {
 		double s = 0.0;
-		for (int ii = 0; ii < powerSpectrum.length; ii++) {
-			s += powerSpectrum[ii];
-		}
+		for (double i : powerSpectrum)
+			s += i;
 		return s;
 	}
 }
